63f5c37246495e1dbe869a2c37c419eebcf245e6
aylimca <ariamekott@gmail.com>
2024-10-07 22:21:11 +0200

    Update and rename 01.php to PiedraPapelTijeras.php

--- PiedraPapelTijeras.php
+++ PiedraPapelTijeras.php
@@ -1,25 +1,14 @@
 <?php
 
-// Caracteres asociadas a las logos 
-// PIEDRA (Puño derecho)
-// PIEDRA2 (Puño Izquierdo / jugador 2 ) 
 define ('PIEDRA',   "&#x1F91C;");
 define ('PIEDRA2',  "&#x1F91B;");
 define ('TIJERAS',  "&#x1F596;");
 define ('PAPEL',    "&#x1F91A;" );
 
-// Tabla de mensajes en función del ganador
 $tmsg = ["¡Empate !",
     " Ha ganado el jugador 1",
     " Ha ganado el jugador 2"];
 
-
-/**
- *  Calcula el ganador 
- *  Parámetros: Dos valores PIEDRA, PAPEL O TIJERA
- *  Resultado: 0 (Empate),1 (1 Gana jugador 1), 2 (Gana jugador 2)   
- *  
- */
 
 function calcularGanador (String $valor1, String $valor2): int{
     
